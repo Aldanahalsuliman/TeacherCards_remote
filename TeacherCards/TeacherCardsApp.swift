1e60fe9288059fa77dcf16d2fef97834c43e739b
Aldana <danat2001as@gmail.com>
2022-12-08 01:43:11 +0300

    Initial Commit

---
+++ TeacherCards/TeacherCardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TeacherCardsApp.swift
+//  TeacherCards
+//
+//  Created by Aldana on 08/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct TeacherCardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
